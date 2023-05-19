271912a3d4a85282189724c737b1538ddca8823a
Mourad <mourad.hadj-ali@etu.univ-lehavre.fr>
2023-05-19 14:20:21 +0200

    Update local files

--- sensor/src/App.js
+++ sensor/src/App.js
@@ -1,14 +1,41 @@
 import './App.css';
 import './Componenet/Header/Header.jsx'
+import './Componenet/Information/Information.jsx'
+
+
+import React from 'react';
+
 import Header from './Componenet/Header/Header.jsx';
+import Information from './Componenet/Information/Information.jsx';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+
+
         <Header/>
       </header>
+
+
+      <information className="App-information">
+
+
+                  <Information/>
+                </information>
     </div>
+
+
+
+
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
